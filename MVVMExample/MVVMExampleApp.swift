003b04b402f90835fb8089b5cc593520e9d19281
Kovvuri Supraj <54736412+kovvurisupraj@users.noreply.github.com>
2025-11-27 15:08:56 -0600

    Initial Commit

---
+++ MVVMExample/MVVMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMExampleApp.swift
+//  MVVMExample
+//
+//  Created by Supraj Kovvuri on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
